22021f5b3010c18ccb75323d4a53c0d9b493fb82
Jesse Todd <jessetodd@rocketmortgage.com>
2022-08-11 15:23:22 -0400

    Adding on week 2 lab 3 for submission

---
+++ StringReversalLab/Program.cs
@@ -0,0 +1,106 @@
+﻿//Reversing a word/full sentence in backwards order:
+Console.WriteLine("Welcome! Please enter a word:");
+string entry = Console.ReadLine();
+ReverseWord(entry);
+
+Console.WriteLine();
+
+//Reversing Sentences while maintaining order: (Extra Challenge)
+Console.WriteLine("Enter a sentence to reverse:");
+string sentence = Console.ReadLine();
+ReverseSentence(sentence);
+
+Console.WriteLine();
+
+Console.WriteLine("Thank you!");
+
+
+//Reversing word/full sentence method
+static void ReverseWord(string word)
+{
+    Stack<char> chars = new Stack<char>();
+
+    for (int i = 0; i < (word.Length); i++)
+    {
+        chars.Push(word[i]);
+    }
+    while (chars.Count > 0)
+    {
+        char c = chars.Pop();
+        Console.Write(c);
+    }
+}
+
+//Reversing full sentence while maintaining order method.
+static void ReverseSentence(string sentence)
+{
+    Stack<char> wordstack = new Stack<char>();
+
+    for (int i = 0; i < sentence.Length; i++)
+    {
+        if (sentence[i] != ' ')
+        {
+            wordstack.Push(sentence[i]);
+        }
+        else
+        {
+            while (wordstack.Count > 0)
+            {
+                Console.Write(wordstack.Pop());
+            }
+            Console.Write(" ");
+        }
+    }
+    //This portion completes the last word
+    while (wordstack.Count > 0)
+    {
+        Console.Write(wordstack.Pop());
+    }
+}
+
+//This code is my past code before it was in Methods.
+//Console.WriteLine("Welcome! Please enter a word:");
+
+//string entry;
+//entry = Console.ReadLine();
+
+////static void ReverseMeth()
+////{ 
+//Stack<char> chars = new Stack<char>();
+
+//for (int i = 0; i < (entry.Length); i++)
+//{
+//    chars.Push(entry[i]);
+//}
+//while (chars.Count > 0)
+//{
+//    char c = chars.Pop();
+//    Console.Write(c);
+//}
+////}
+//Console.WriteLine();
+
+////Reversing Sentences
+//Console.WriteLine("Enter a sentence to reverse:");
+//string sentence = Console.ReadLine();
+//Stack<char> wordstack = new Stack<char>();
+
+//for (int i = 0; i < sentence.Length; i++)
+//{
+//    if (sentence[i] != ' ')
+//    {
+//        wordstack.Push(sentence[i]);
+//    }
+//    else
+//    {
+//        while (wordstack.Count > 0)
+//        {
+//            Console.Write(wordstack.Pop());
+//        }
+//        Console.Write(" ");
+//    }
+//}
+//while (wordstack.Count > 0)
+//{
+//    Console.Write(wordstack.Pop());
+//}
---
+++ Week2Exercises/Program.cs
@@ -0,0 +1,15 @@
+﻿//Exercise 23
+//Console.WriteLine("Please enter some text:");
+//string entry = Console.ReadLine();
+//Console.WriteLine($" The first 10 characters are: {entry.Substring(0,10)}"); //needs to be 10 because 10 not inclusive. (up to 10)
+//add continue method
+
+//Exercise 24
+//Console.WriteLine("Please enter some text (10 characters or more:");
+//string entry = Console.ReadLine();
+//Console.WriteLine(entry.Substring(entry.Length - 10));
+
+//Exercise 25
+Console.WriteLine("Please enter a sentence:");
+string entry = Console.ReadLine();
+Console.WriteLine(entry.Split());
